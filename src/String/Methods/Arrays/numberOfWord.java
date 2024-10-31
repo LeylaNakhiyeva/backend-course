92d55c61ee85c579cb33498fee3ad42f9eebaa0a
Leyla <leylanaqiyeva940@gmail.com>
2024-10-31 12:34:10 +0400

    reverseWord

---
+++ src/String/Methods/Arrays/numberOfWord.java
@@ -0,0 +1,12 @@
+package String.Methods.Arrays;
+
+import java.util.Scanner;
+
+public class numberOfWord {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String sentence= sc.next();
+
+    }
+    public static void
+}
---
+++ src/String/Methods/Arrays/reverseWord.java
@@ -0,0 +1,19 @@
+package String.Methods.Arrays;
+
+import java.util.Scanner;                       //task7
+
+public class reverseWord {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String word= sc.next();
+        reverseWord(word);
+    }
+    public static void reverseWord(String word){
+       String result="";
+        for(int i=word.length()-1;i>=0; i--){
+            result+=word.charAt(i);
+        }
+        System.out.println("result= "+result);
+    }
+}

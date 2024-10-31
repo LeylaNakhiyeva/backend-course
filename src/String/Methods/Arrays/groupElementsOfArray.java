c77e78fe4714e0429c056f6bce993b53b2362078
Leyla <leylanaqiyeva940@gmail.com>
2024-10-31 15:00:38 +0400

    arrayin elementlerini qruplasdir

---
+++ src/String/Methods/Arrays/groupElementsOfArray.java
@@ -0,0 +1,29 @@
+package String.Methods.Arrays;
+
+public class groupElementsOfArray {
+    public static void main(String[] args) {
+        int [] array={5,12,19,22,8,25,30};
+        groupElementsOfArray(array);
+    }
+    public static void groupElementsOfArray(int [] arr){
+        int a=0, b=0, c=0;
+        for (int i=0; i< arr.length; i++){
+            if(arr[i]>=0 && arr[i]<=10){
+                a++;
+            }
+        }
+        System.out.println("[0;10]: "+a+" element");
+        for (int i=0; i< arr.length; i++){
+            if(arr[i]>=11 && arr[i]<=20){
+                b++;
+            }
+        }
+        System.out.println("[11;20]: "+b+"element");
+        for (int i=0; i< arr.length; i++){
+            if(arr[i]>=21 && arr[i]<=30){
+                c++;
+            }
+        }
+        System.out.println("[21:30]: "+c+"element");
+    }
+}
--- src/String/Methods/Arrays/numberOfWord.java
+++ src/String/Methods/Arrays/numberOfWord.java
@@ -4,9 +4,29 @@ import java.util.Scanner;
 
 public class numberOfWord {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String sentence= sc.next();
-
+        Scanner sc = new Scanner(System.in);
+        String sentence = sc.next();
+        numberOfWord(sentence);
     }
-    public static void
-}
+
+    public static void numberOfWord(String sentence) {
+        int k = 0;
+
+        for (int i = 0; i < sentence.length(); i++) {
+           if(sentence.charAt(i)==' ') {
+                k++;
+            }
+        }
+        System.out.println(k);
+    }}
+
+
+
+
+//        String  []  b=sentence.split(" ");
+//                    k= b.length;
+//        System.out.println(k);
+//        return k;
+//    }
+//}
+//}
